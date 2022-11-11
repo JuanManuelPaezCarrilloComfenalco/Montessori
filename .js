2d0614b8b5cb9f4231bb7748bb7c96a56b26f50b
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-11 18:18:08 -0500

    Astrid

---
+++ .js
@@ -0,0 +1,9 @@
+addEventListener("DOMContentLoaded", (e)=> {
+
+    alert("Hola Atrid, ¿como estas?")
+    prompt("escribe tu respuesta")
+    alert("Te invito a checkear tu pagina web")
+    confirm("Recuerda que al final de la pagina hay un LINK donde podras acceder a mas información sobre colegios MONTESSORI")
+    alert("¡BIENVENIDA!")
+    
+    })
